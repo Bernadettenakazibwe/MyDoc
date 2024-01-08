0d44b0340d7797e24b738fafac9c5ed86ede7adc
Bernadette Nakazibwe <bernadettenakazibwe@gmail.com>
2024-01-08 15:42:32 +0100

    my doc

---
+++ sql/doctors.sql
@@ -0,0 +1,7 @@
+CREATE TABLE patients (
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(191) NOT NULL,
+    email VARCHAR(191) NOT NULL,
+    phone VARCHAR(191) NOT NULL,
+    Diagnosis VARCHAR(191) NOT NULL
+)
